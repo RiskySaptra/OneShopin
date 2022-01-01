4800d9dccea6b3a02ed06642da4f470c9bc309ea
kiki014045@gmail.com <kiki014045@gmail.com>
2022-01-02 02:10:37 +0700

    update config

--- src/config.js
+++ src/config.js
@@ -3,15 +3,18 @@ import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
 
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBp0TeJ2RFijs3g0GMiIn6VTpvK4d4l7SU",
-  authDomain: "dashboard-ocr.firebaseapp.com",
-  databaseURL: "https://dashboard-ocr.firebaseio.com",
-  projectId: "dashboard-ocr",
-  storageBucket: "dashboard-ocr.appspot.com",
-  messagingSenderId: "464455043732",
-  appId: "1:464455043732:web:8a736a4120b57489078281"
+  apiKey: "AIzaSyAL-oJAolT1uOWgMPkippzV3z68D1ZRMD8",
+  authDomain: "money-experiment-6aac5.firebaseapp.com",
+  projectId: "money-experiment-6aac5",
+  storageBucket: "money-experiment-6aac5.appspot.com",
+  messagingSenderId: "1045279180074",
+  appId: "1:1045279180074:web:cf75b09adf351c20f1ca53",
+  measurementId: "G-8803W4HQ8P"
 };
+
 firebase.initializeApp(firebaseConfig);
 
 export const auth = firebase.auth;
